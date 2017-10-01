90c9233af5596c2df0de216df94d548c6dd495fb
Arthur Arts <arart@fundeon.nl>
2017-10-01 22:54:55 +0200

    autofill h2 database on startup

--- src/main/java/it/codedvalue/springboot/campaign/service/CampaignService.java
+++ src/main/java/it/codedvalue/springboot/campaign/service/CampaignService.java
@@ -1,14 +1,10 @@
 package it.codedvalue.springboot.campaign.service;
 
-import javax.annotation.Resource;
-
 import it.codedvalue.springboot.campaign.domain.Campaign;
 import it.codedvalue.springboot.campaign.repository.CampaignRepository;
 import java.time.LocalDate;
-import java.time.Month;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
-
 
 @Service
 public class CampaignService {
@@ -20,35 +16,8 @@ public class CampaignService {
 
     }
 
-
     public Flux<Campaign> getActiveCampaignsOnReferenceDate(final LocalDate referenceDate) {
 
-
-/*
-        campaignRepository.findAll();
-
-        Campaign campaign1 = Campaign.builder()
-                .name("click and win")
-                .code("caw")
-                .startDate(LocalDate.of(2017, Month.SEPTEMBER, 30))
-                .endDate(LocalDate.of(2017, Month.OCTOBER, 3))
-                .build();
-
-        Campaign campaign2 = Campaign.builder()
-                .name("click and win")
-                .code("caw")
-                .startDate(LocalDate.of(2017, Month.SEPTEMBER, 30))
-                .endDate(LocalDate.of(2017, Month.NOVEMBER, 1))
-                .build();
-
-        Campaign campaign3 = Campaign.builder()
-                .name("click and win")
-                .code("caw")
-                .startDate(LocalDate.of(2017, Month.NOVEMBER, 1))
-                .endDate(LocalDate.of(2017, Month.NOVEMBER, 30))
-                .build();
-*/
-        
         return Flux.fromIterable(campaignRepository.findAll())
                 .filter(c -> referenceDate.isAfter(c.getStartDate()) && referenceDate.isBefore(c.getEndDate()));
     }
